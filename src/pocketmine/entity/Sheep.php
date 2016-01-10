fff53f6fa341f1ef528fcde8599cf5691014b581
Meshida <meshida.200202@gmail.com>
2016-01-10 16:13:07 +0900

    complete

---
+++ src/pocketmine/entity/Sheep.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ *
+ *  _                       _           _ __  __ _             
+ * (_)                     (_)         | |  \/  (_)            
+ *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___  
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \ 
+ * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/ 
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___| 
+ *                     __/ |                                   
+ *                    |___/                                                                     
+ * 
+ * This program is a third party build by ImagicalMine.
+ * 
+ * PocketMine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ImagicalMine Team
+ * @link http://forums.imagicalcorp.ml/
+ * 
+ *
+*/
+
+namespace pocketmine\entity;
+
+use pocketmine\item\Item;
+
+class Sheep extends Animal implements Colorable{
+ 
+      public function getName() {
+       return "Sheep";
+      }
+      
+      public function getDrops(){
+           $drops = Item::get(Item::WOOL, mt_rand(1, 15), 1);//haven't found Network IDs for coloured sheeps (not wools) so can't check the color of the sheep.
+           return $drops;
+      }
+}
