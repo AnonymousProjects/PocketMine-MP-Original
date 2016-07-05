76de7afb527775d5e14317581083741e20cfc848
AnonymousProjects <meshida.200202@gmail.com>
2016-07-05 17:22:40 +0900

    a few updates

--- start.cmd
+++ start.cmd
@@ -1,16 +1,16 @@
 @echo off
-TITLE Genisys server software for Minecraft: Pocket Edition
+TITLE PocketMine server software for Minecraft: Pocket Edition
 cd /d %~dp0
 
 if exist bin\php\php.exe (
 	set PHPRC=""
 	set PHP_BINARY=bin\php\php.exe
-) else (
+	) else (
 	set PHP_BINARY=php
-)
+	)
 
-if exist Genisys*.phar (
-	set POCKETMINE_FILE=Genisys*.phar
+if exist PocketMine-MO*.phar (
+	set POCKETMINE_FILE=PocketMine-MO*.phar
 ) else (
     if exist PocketMine-MP.phar (
         set POCKETMINE_FILE=PocketMine-MP.phar
@@ -18,10 +18,10 @@ if exist Genisys*.phar (
 	    if exist src\pocketmine\PocketMine.php (
 	        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
         ) else (
-            if exist Genisys.phar (
-                set POCKETMINE_FILE=Genisys.phar
+            if exist PocketMine-MO.phar (
+                set POCKETMINE_FILE=PocketMine-MO.phar
             ) else (
-		        echo "[ERROR] Couldn't find a valid Genisys installation."
+		        echo "[ERROR] Couldn't find a valid PocketMine-MP installation."
 		        pause
 		        exit 8
 		    )
@@ -30,7 +30,7 @@ if exist Genisys*.phar (
 )
 
 if exist bin\mintty.exe (
-	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-iTX" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-) else (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=12 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MO" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+	) else (
 	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
-)
+	)
